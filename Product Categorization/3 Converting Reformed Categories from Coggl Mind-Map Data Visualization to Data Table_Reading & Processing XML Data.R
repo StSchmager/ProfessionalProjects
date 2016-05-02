d6957165f2eae9137d58715341787ae254cba42a
Stefan Schmager <schmager.stefan@gmail.com>
2016-05-02 15:12:40 -0500

    Read all leaf-node categories

--- Product Categorization/3 Converting Reformed Categories from Coggl Mind-Map Data Visualization to Data Table_Reading & Processing XML Data.R
+++ Product Categorization/3 Converting Reformed Categories from Coggl Mind-Map Data Visualization to Data Table_Reading & Processing XML Data.R
@@ -1,7 +1,7 @@
 # 1) Download Raw (XML) Data ###########################################################################################
 
 # View mindmap on Coggle: https://coggle.it/diagram/VxZRI2-gIRhJZNvh
-# Download this Coggl mindmap as .mm File and name file
+# Download this mindmap as .mm File and name file
 FileName <- "Product Categories.mm"
 # Save into working directory and read file from there
 setwd("C:/Users/Stefan/Google Drive/Projects/3 Transaction-Data Analyses/0 Org/3 Market Basket Analysis/Product Categorization")
@@ -12,11 +12,17 @@ setwd("C:/Users/Stefan/Google Drive/Projects/3 Transaction-Data Analyses/0 Org/3
 library(XML)
 library(plyr)
 
-# Read XML data and convert to R dataobjects
-setwd("C:/Users/Stefan/Google Drive/Projects/3 Transaction-Data Analyses/0 Org/3 Market Basket Analysis/Product Categorization")
+## Read XML data and convert to R dataobjects
+
+# Read complete XML tree
 CategoryTree      <- xmlInternalTreeParse(FileName, getDTD = F)
 CategoryNodes     <- getNodeSet(CategoryTree, "//*[@TEXT]")
-CategoryList      <- lapply(CategoryNodes, xpathApply, path = './/*[@TEXT]', xmlGetAttr, "TEXT", default = NA)
+
+# Select all nodes
+AllNodes          <- xpathSApply(CategoryTree, '//node', xmlGetAttr, "TEXT", default = NA)
+# Select only the leave nodes (nodes without child nodes)
+LeaveNodes        <- xpathSApply(CategoryTree, '//node[not(node)]', xmlGetAttr, "TEXT", default = NA)
+LeaveNodeSet      <- getNodeSet( CategoryTree, '//node[not(node)]')
 
 # 3) Export Processed Data #############################################################################################
 
